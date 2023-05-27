3b48533f27403a603a0b4481d2d17b30502f9a12
RiggedProjects <66296627+RiggedProjects@users.noreply.github.com>
2023-05-27 12:06:53 -0500

    02_repeatString My Solution

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,19 @@
-const repeatString = function() {
+const repeatString = function(string, num) {
+    count = 0;
+    message = '';
 
+    if (string === '') {
+        message = '';
+    } else if (num < 0) {
+        message = 'ERROR';
+    } else {
+        while (count < num) {
+            message = message + 'hey';
+            count++;
+        }
+    }
+    
+    return message;
 };
 
 // Do not edit below this line
