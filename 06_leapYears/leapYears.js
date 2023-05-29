6ed0f324b02d73c889b2cdc5631dc7ffb0fd17d2
RiggedProjects <66296627+RiggedProjects@users.noreply.github.com>
2023-05-29 18:38:47 -0500

    09_leapYears My Solution

--- 06_leapYears/leapYears.js
+++ 06_leapYears/leapYears.js
@@ -1,5 +1,17 @@
-const leapYears = function() {
-
+const leapYears = function(year) {
+    //"unless they are divisible by 400"
+    if (Number.isInteger(year / 400)) {
+        return true;
+    //"years divisible by 100 are not leap years"
+    } else if (Number.isInteger(year / 100)) {
+        return false;
+    //"Leap years are years divisible by four"
+    } else if (Number.isInteger(year / 4)) {
+        return true;
+    //Not a leap year.
+    } else {
+        return false;
+    }
 };
 
 // Do not edit below this line
