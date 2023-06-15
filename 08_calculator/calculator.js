907b3766ca0b657a6c0ef4cb766bdceca095a01f
RiggedProjects <66296627+RiggedProjects@users.noreply.github.com>
2023-06-15 16:50:35 -0500

    08_calculator My Solutions

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,45 @@
-const add = function() {
-	
+const add = function(a, b) {
+	return a + b;
 };
 
-const subtract = function() {
-	
+const subtract = function(a, b) {
+	return a - b;
 };
 
-const sum = function() {
-	
+const sum = function(num) {
+	let total = 0;
+
+  for (let n of num) {
+    total += n;
+  }
+  return Number(total);
 };
 
-const multiply = function() {
+const multiply = function(num) {
+  let total = 1;
 
+  for (let n of num) {
+    total *= n;
+  }
+  return Number(total);
 };
 
-const power = function() {
-	
+const power = function(a, b) {
+  let o = a;
+  while (b > 1) {
+    a *= o;
+    --b;
+  }
+  return Number(a);
 };
 
-const factorial = function() {
-	
+const factorial = function(num) {
+  let total = 1;
+  while (num > 0) {
+    total *= num;
+    --num;
+  }
+  return total;
 };
 
 // Do not edit below this line
