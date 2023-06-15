4cba44e0bf43ea0b3c3d80478fca139c03e58064
RiggedProjects <66296627+RiggedProjects@users.noreply.github.com>
2023-06-15 18:47:15 -0500

    09_palindromes My Solution

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,20 @@
-const palindromes = function () {
+const palindromes = function (string) {
+    let newString = "";
+    let storedString = string.replace(/[^\w\d]/g, "").toLowerCase();
+    let length = storedString.length;
+    string = storedString;
 
+    while (length > 0) {
+        newString += string.slice(-1);
+        string = string.substring(0,string.length-1);
+        length--;
+    }
+    
+    if (storedString === newString) {
+        return true;
+    } else {
+        return false;
+    }
 };
 
 // Do not edit below this line
