fba4a445b877b410b0f28dfd68f4ce615e8ad6c8
RiggedProjects <RiggedProjects@gmail.com>
2023-06-16 07:48:44 -0500

    10_fibonacci My First Solution

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,20 @@
-const fibonacci = function() {
+const fibonacci = function(n) {
+    let index = 0;
+    let fib = [ 1, 1 ];
+    
+    while (index < 30) {
+        let sum = fib[fib.length - 1] + fib[fib.length - 2];
+        fib.push(sum);
+        index++;
+    }
 
+    let value = fib[Number(n) - 1];
+
+    if (value > 0) {
+        return value;
+    } else {
+        return 'OOPS';
+    }
 };
 
 // Do not edit below this line
